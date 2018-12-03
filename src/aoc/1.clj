faf5bbf9cbeea37ac392a6fde47ba8e82bfcb816
Waatze Goldewijk <waatze.goldewijk@nedap.com>
2018-12-03 23:45:37 +0100

    Day 2 - second puzzle

--- src/aoc/1.clj
+++ src/aoc/1.clj
@@ -1,5 +1,20 @@
 (ns aoc.1
- (:require [clojure.java.io :as io] ) )
+  (:require [clojure.java.io :as io]))
 
 (with-open [rdr (clojure.java.io/reader (io/resource "1_frequency_shifts.txt"))]
   (println "Frequency:" (reduce + (map #(Integer/parseInt %) (line-seq rdr)))))
+
+(defn duplicate_frequency_filter
+  ([freq_seq] (duplicate_frequency_filter freq_seq 0 #{}))
+  ([frequencies current_frequency previous_frequencies]
+   (let [new_frequency (+ (first frequencies) current_frequency)]
+     (if
+       (contains? previous_frequencies new_frequency)
+       new_frequency
+       (recur (rest frequencies) new_frequency (conj previous_frequencies new_frequency))))
+    )
+  )
+
+(with-open [rdr (clojure.java.io/reader (io/resource "1_frequency_shifts.txt"))]
+  (println "First repeated frequency:" (duplicate_frequency_filter (cycle (map #(Integer/parseInt %) (line-seq rdr)))))
+  )
